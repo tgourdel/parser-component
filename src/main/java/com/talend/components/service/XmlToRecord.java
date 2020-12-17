a39bffcacb6618fa2f1b0227bc2621a86947db15
Thibaut Gourdel <tgourdel@talend.com>
2020-12-16 23:33:40 -0800

    Switch Dom to Xpath

--- src/main/java/com/talend/components/service/XmlToRecord.java
+++ src/main/java/com/talend/components/service/XmlToRecord.java
@@ -7,6 +7,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.json.*;
+import javax.xml.xpath.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
@@ -26,29 +27,38 @@ public class XmlToRecord implements Serializable {
         this.factory = factory;
     }
 
-    public Record toRecord(final Node node) {
+    public Record toRecord(final Node node) throws XPathExpressionException {
 
         System.out.println("====> Node name: " + node.getNodeName());
         System.out.println("====> node value: " + node.getNodeValue());
         Record.Builder builder = factory.newRecordBuilder();
 
         // get all child nodes
-        NodeList childrens = node.getChildNodes();
+        NodeList doc = node.getChildNodes();
 
         if(node.hasChildNodes()) {
-            System.out.println("====> Node has childrens!! ");
-            for (int i=0; i<childrens.getLength(); i++) {
-                // get child node
-                Node childNode = childrens.item(i);
-                System.out.println("====> Child node found!");
-                System.out.println("====> Child node name: " + childNode.getNodeName());
-                System.out.println("====> Child node value: " + childNode.getNodeValue());
 
-                builder.withRecord(childNode.getNodeName(), toRecord(childNode));
+            System.out.println("====> Node has childrens!! ");
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            XPath xpath = xpathFactory.newXPath();
+            XPathExpression expr = xpath.compile("//*");
+
+            NodeList elements = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+            for(int i=0; i < elements.getLength(); ++i) {
+                System.out.println("====> Node num " + i);
+                System.out.println("====> Name " + elements.item(i).getNodeName());
+                builder.withRecord(elements.item(i).getNodeName(), toRecord(elements.item(i)));
             }
         }
         else if (node.getNodeType() == Node.TEXT_NODE) {
             System.out.println("====> Node is text ");
+
+            String name = node.getNodeName();
+            String value = node.getTextContent();
+
+            System.out.println("====>Name : " + name);
+            System.out.println("====>Value : " + value);
 
             builder.withString(node.getNodeName(), node.getNodeValue());
         }
